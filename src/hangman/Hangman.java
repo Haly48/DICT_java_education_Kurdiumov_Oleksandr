34c4ae0348fb3da057d6dc71e301d56fae656ad6
haly <o.d.kurdiumov@student.khai.edu>
2021-12-26 01:22:42 +0200

    Hangman 3rd stage

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,13 +1,16 @@
 package hangman;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
+    static String[] Words = { "python", "java", "javascript", "kotlin" };
     public static void main(String[] args) {
+        String ans = Words[new Random().nextInt(Words.length)];
         System.out.println("HANGMAN");
         Scanner input = new Scanner(System.in);
+        System.out.print("Guess the word: > ");
         String word = input.next();
-        String ans = "java";
         if (word.equals(ans))
         {
             System.out.println("You survived!");
