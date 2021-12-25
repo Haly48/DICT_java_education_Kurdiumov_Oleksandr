376c649f578067d9060d6903df289aae1a7cf1b2
haly <o.d.kurdiumov@student.khai.edu>
2021-12-25 20:05:14 +0200

    ChatBot 1st stage

--- src/ChatBot.java
+++
@@ -1,6 +0,0 @@
-public class ChatBot {
-    public static void main(String[] args) {
-        System.out.println("Hello my name is Juan");
-        System.out.println("I was created in 2021.");
-    }
-}
